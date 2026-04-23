51bd6f7eca4fd426d696500c58c10beba26aaedf
HIGOR MACHADO MIRANDA <12421698650@campus.machado.mch>
2026-04-23 13:27:50 -0300

    initial commit

---
+++ src/main/java/br/edu/ifsuldeminas/supermercado/servico/WebConstante.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package br.edu.ifsuldeminas.supermercado.servico;
+
+/**
+ *
+ * @author 12421698650
+ */
+public class WebConstante {
+      public static final String BASE_PATH = "/com/mycompany/higormiranda/controlador";
+
+
+}
